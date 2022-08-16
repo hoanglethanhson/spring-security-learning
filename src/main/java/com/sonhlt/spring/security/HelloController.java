8397944150ab7956d0a4215e2c15937926e6143e
leson2912 <leson1998ls@gmail.com>
2022-08-17 01:01:39 +0700

    first commit

---
+++ src/main/java/com/sonhlt/spring/security/HelloController.java
@@ -0,0 +1,12 @@
+package com.sonhlt.spring.security;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping("/hello")
+    public String hello() {
+        return "Spring Security";
+    }
+}
